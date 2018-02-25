ee950d523e9c655dcfa5dc67a98d100b686c19a7
Damien Bevins <DBevins@student.neumont.edu>
2018-02-25 14:04:34 -0700

    Updates to the index.js and layout.pug

--- index.js
+++ index.js
@@ -3,3 +3,45 @@ var exp = require('express'),
    bp = require('body-parser'),
    es = require('express-session'),
    path = require('path');
+
+app.get('/', function(req, res){
+   res.render('home', {
+      "title" : "Home"
+   });
+});
+
+app.get('/admin', function(req, res){
+   res.render('admin', {
+      "title" : ""
+   });
+});
+
+app.get('/login', function(req, res){
+   res.render('login', {
+      "title" : ""
+   });
+});
+
+app.get('/board', function(req, res){
+   res.render('board', {
+      "title" : ""
+   });
+});
+
+app.get('/topics', function(req, res){
+   res.render('topics', {
+      "title" : ""
+   });
+});
+
+app.get('/edit', function(req, res){
+   res.render('edit', {
+      "title" : ""
+   });
+});
+
+app.get('/register', function(req, res){
+   res.render('edit', {
+      "title" : ""
+   });
+});
